ac850b1308f3cde8e63990858357ed622ce50648
buselivan-dot <buselivan21@gmail.com>
2026-05-31 21:51:01 +0200

    initial commit

---
+++ eventBooking/src/test/java/com/system/eventBooking/EventBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.eventBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
